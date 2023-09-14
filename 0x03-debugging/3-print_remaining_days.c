9d8553d99b1be7e51fe4c64b0e5c5e45fa125714
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-14 07:04:17 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,27 +15,27 @@ void print_remaining_days(int month, int day, int year)
 	 * leep year when if can be dived
 	 * by 100 and 400 or by 4 evenly
 	 */
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
+	if ((year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
+			{
+			if (month >= 2 && day >= 60)
+			{
 			day++;
-		}
+			}
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+			}
+			else
+			{
+			if (month == 2 && day == 60)
+			{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
+			}
+			else
+			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+			}
+			}
 }
 
