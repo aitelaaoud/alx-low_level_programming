1fb9e23ac921d239e8ebcd2a31098114a2df2cf1
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-14 08:53:08 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase char
+ * @c : ascii value
+ *
+ * Return: if success 1
+ * and 0 if there is an error
+ */
+
+int _isupper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+		return (1);
+	else
+
+		return (0);
+
+	return (0);
+}
