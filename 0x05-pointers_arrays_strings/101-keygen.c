2b0b12fe4dae74af36e657404a2c66690474e4c9
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-18 12:45:42 -0700

    random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point generates a random password
+ *
+ * Return: the generated password
+ */
+int main(void)
+{
+	char c;
+	int sum;
+
+	srand(time(0));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
