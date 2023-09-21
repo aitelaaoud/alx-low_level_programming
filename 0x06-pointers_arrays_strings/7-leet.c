300108d2969a84a83fdc5679eff20c7b149778b3
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-21 08:44:38 -0700

    letters replaced by numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * Return: pointer to resulting string
+ * @str: string to be encoded
+ */
+char *leet(char *str)
+{
+	int i, j;
+
+	char alpha[80] = "aAeEoOtTlL";
+	char numbers[80] = "43071";
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = numbers[j / 2];
+			}
+		}
+	}
+	return (str);
+}
