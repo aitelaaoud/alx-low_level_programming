87f33912b925f8233b40dbec67e96bccf3445b7a
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-21 09:00:42 -0700

    Rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,29 +7,21 @@
  */
 char *rot13(char *str)
 {
-	int i = 0, j;
-	char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		'k', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-		'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-		'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's',
-		't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int i, j;
+	char input[80] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[80] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (str[i])
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; input[j] != '\0'; j++)
 		{
-			if (str[i] == alpha[j])
+			if (str[i] == input[j])
 			{
-				str[i] = rot13[j];
+				str[i] = output[j];
 				break;
 			}
 		}
-		i++;
+
 	}
 	return (str);
 }
