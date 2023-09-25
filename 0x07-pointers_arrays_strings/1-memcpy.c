ff10f961828e5ac0d62f17fc99816b74d19761e9
aitelaaoud <ahmed.contact.18@gmail.com>
2023-09-25 05:54:56 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory erea
+ * @dest: dest buffer
+ * @src: src buffer
+ * @n: the number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
